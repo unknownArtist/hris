5aa64161e1e1a7fdbfa1382bb57f41e6b9b9312e
Muhammad Saqib <nayab.rajpoot@gmail.com>
2012-09-04 16:20:02 +0500

    changes in bootstrap and plugins added

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,13 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+		protected function _initAutoload()
+		{
+			// $fc = Zend_Controller_Front::getInstance();
+			// $fc->registerPlugin(new Plugin_AccessCheck());
 
+
+		}
 
 }
 
