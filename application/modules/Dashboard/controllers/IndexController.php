535106a05b8195bb7990bbd715c864d565b9bd36
sehrish habib <sehrish@gmail.com>
2012-09-04 04:14:35 -0700

    login

--- application/modules/Dashboard/controllers/IndexController.php
+++ application/modules/Dashboard/controllers/IndexController.php
@@ -6,15 +6,40 @@ class Dashboard_IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+       
 
     }
+   
 
     public function indexAction()
     {
 
-        // $dd = new Dashboard_Model_Login();
-        // print_r($dd->fetchAll()->toArray());
-        $this->view->form = new Dashboard_Form_Login();
+        
+        $members = new Dashboard_Model_Login();
+        $form = new Dashboard_Form_Login();
+        $this->view->form = $form;
+        if($this->getRequest()->isPost()){
+            if($form->isValid($_POST)){
+                $data = $form->getValues();
+                $auth = Zend_Auth::getInstance();
+                $authAdapter = new Zend_Auth_Adapter_DbTable($members->getAdapter(),'members');
+                $authAdapter->setIdentityColumn('userName')
+                            ->setCredentialColumn('password');
+                $authAdapter->setIdentity($data['userName'])
+                            ->setCredential($data['password']);
+                $result = $auth->authenticate($authAdapter);
+                if($result->isValid()){
+                    $storage = new Zend_Auth_Storage_Session();
+                    $storage->write($authAdapter->getResultRowObject(array('memberID', 'userName','role')));
+                    $this->view->successMsg = "you are logged in";
+
+                } else {
+                    $this->view->errorMessage = "Invalid username or password. Please try again.";
+                }         
+            }
+        }
+      
+
 
     }
 
